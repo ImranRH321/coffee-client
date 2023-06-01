d0c226a27f7e3d480702916fa54b58235e94ed21
ImranRH321 <imranrh247@gmail.com>
2023-06-01 21:07:42 +0600

    coffee project stating

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-secondary my-3 text-3xl text-center '>App Components</h1>
+    </div>
+  );
+};
+
+export default App;
